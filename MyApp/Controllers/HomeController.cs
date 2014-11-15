be43d08def7d15b1ddc406c4f007b975ccd694a8
Matthew Lowe <matt@literateprogrammer.com>
2014-11-15 12:50:30 -0600

    need an actual site here

---
+++ MyApp/Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ThingDbContext db = new ThingDbContext();
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(db.Things.ToList());
+        }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Details(long id = 0)
+        {
+            Thing thing = db.Things.Find(id);
+            if (thing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thing);
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        public ActionResult Create(Thing thing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Things.Add(thing);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(thing);
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(long id = 0)
+        {
+            Thing thing = db.Things.Find(id);
+            if (thing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thing);
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Thing thing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thing).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(thing);
+        }
+
+        //
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(long id = 0)
+        {
+            Thing thing = db.Things.Find(id);
+            if (thing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thing);
+        }
+
+        //
+        // POST: /Home/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Thing thing = db.Things.Find(id);
+            db.Things.Remove(thing);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
