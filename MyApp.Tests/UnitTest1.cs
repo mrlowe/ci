24f4e6aea8ac59c60e3454d31488e492245cf0b2
Matthew Lowe <matt@literateprogrammer.com>
2014-11-15 11:03:05 -0600

    moving to top level directory

---
+++ MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyApp.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ThingTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+    }
+}
